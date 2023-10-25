7805722b464cf7ce40333da56801b9a7db7ade0b
Nitin Kumar Pal <nitinpal900@gmail.com>
2023-10-25 10:11:36 +0530

    bubbleSort'

---
+++ bubbleSort.java
@@ -0,0 +1,34 @@
+package practice;
+
+public class bubbleSort {
+	
+	public static void printArray(int[] arr) {
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
+	public static void bubbleSort(int[] arr) {
+		for(int i = arr.length-2; i>=0; i--) {
+			boolean swapped = false;
+			for(int j=0; j<=i; j++) {
+				if(arr[j]>arr[j+1]) {
+					swapped = true;
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+			if(swapped == false) {
+				break;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {5,1,4,6,7,2};
+		bubbleSort(arr);
+		printArray(arr);
+	}
+
+}
