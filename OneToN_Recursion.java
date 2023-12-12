0364ffa2a50c66b15b8133c77adcb4e0779a6a8a
Nitin Kumar Pal <nitinpal900@gmail.com>
2023-12-12 12:25:12 +0530

    changes

--- OneToN_Recursion.java
+++ OneToN_Recursion.java
@@ -4,7 +4,18 @@ public class OneToN_Recursion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int n = 5;
+		print(n);
+	}
 
+	public static void print(int n) {
+		// TODO Auto-generated method stub
+		if(n==1) {
+			System.out.println(n);
+			return;
+		}
+		print(n-1);
+		System.out.println(n);
 	}
 
 }
